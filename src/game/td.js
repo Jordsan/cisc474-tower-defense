428c3b533de524a56d4cd04ec3a8d257ef533945
Jordsan <mail.jordan.michaels@gmail.com>
2017-09-18 16:30:15 -0400

    basic map generation

---
+++ src/game/td.js
@@ -0,0 +1,23 @@
+var mapArray = [];
+for(var i = 0; i < 10; i++){
+    mapArray[i] = [];    
+    for(var j = 0; j < 10; j++){ 
+        mapArray[i][j] = 0;    
+    }    
+}
+
+$( document ).ready(function() {
+    var mapHtml = "";
+    for(var i = 0; i < mapArray.length; i++){
+        mapHtml += '<div id="row-' + i + '">';
+        for(var j = 0; j < mapArray[i].length; j++){ 
+            mapHtml += '<span id="' + i + '-' + j + '">' + mapArray[i][j] + '</span>';
+            console.log("hit");
+        }
+        mapHtml += '</div>';    
+    }
+    
+    $("#map-div").html(mapHtml);
+        
+    
+});
