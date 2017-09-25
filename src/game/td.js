4a00e57faab9862e18297e3edb31ea7b0ceffe82
james <jgeisler720@gmail.com>
2017-09-25 17:41:06 -0400

    james update

--- src/game/td.js
+++ src/game/td.js
@@ -14,9 +14,9 @@ var gameTime = 0;
 $( document ).ready(function() {
     var mapHtml = "";
     for(var i = 0; i < mapArray.length; i++){
-        mapHtml += '<div id="row-' + i + '">';
+        mapHtml += '<div class="row" id="row-' + i + '">';
         for(var j = 0; j < mapArray[i].length; j++){ 
-            mapHtml += '<span id="' + i + '-' + j + '">' + mapArray[i][j] + '</span>';
+            mapHtml += '<span class="column" id="' + i + '-' + j + '">' + mapArray[i][j] + '</span>';
             console.log("hit");
         }
         mapHtml += '</div>';
